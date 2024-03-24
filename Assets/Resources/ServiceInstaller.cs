7fb60f894a774996fed7e753dcad8207bb82a1b4
xanixon <xanixon@mail.ru>
2024-03-24 22:39:27 +0300

    Updating Bootstrapper

--- Assets/Resources/ServiceInstaller.cs
+++ Assets/Resources/ServiceInstaller.cs
@@ -4,8 +4,14 @@ using Zenject;
 public class ServiceInstaller : MonoInstaller
 {
     [SerializeField] private WorldMotor _worldMotor;
+    [SerializeField] private SceneObjectsSpawner _decorationSpawner;
+    [SerializeField] private SceneObjectsSpawner _waterSpawner;
+    [SerializeField] private ScoreHandler _scoreHandler;
     public override void InstallBindings()
     {
         Container.Bind<WorldMotor>().FromComponentInNewPrefab(_worldMotor).AsSingle().Lazy();
+        Container.Bind<SceneObjectsSpawner>().WithId("DecorationsSpawner").FromComponentInNewPrefab(_decorationSpawner).AsCached().Lazy();
+        Container.Bind<SceneObjectsSpawner>().WithId("WaterSpawner").FromComponentInNewPrefab(_waterSpawner).AsCached().Lazy();
+        Container.Bind<ScoreHandler>().FromComponentInNewPrefab(_scoreHandler).AsSingle().Lazy();
     }
 }
