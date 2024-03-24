b8ac316b182ee90d8c1b8933a92a31bd755d70a4
xanixon <xanixon@mail.ru>
2024-03-24 22:37:00 +0300

    adding ScoreHandler

---
+++ Assets/Scripts/ScoreHandler/ScoreHandler.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreHandler : MonoBehaviour
+{
+    public int Score { get; private set; }
+    public event Action<int> OnScoreChanged;
+    public void AddScore(int value)
+    {
+        if (value < 0) return;
+        Score += value;
+        OnScoreChanged?.Invoke(Score);
+    }
+}
