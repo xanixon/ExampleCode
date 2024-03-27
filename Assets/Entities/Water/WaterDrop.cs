0c13296f0fac20ef8f8b1d2640ace0689752c4c7
xanixon <xanixon@mail.ru>
2024-03-27 14:28:10 +0300

    Adding water death effect

--- Assets/Entities/Water/WaterDrop.cs
+++ Assets/Entities/Water/WaterDrop.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class WaterDrop : MonoBehaviour
 {
     [SerializeField] private GameObject _deathEffect;
+    [SerializeField] private AudioSource _source;
+    [SerializeField]
+    private float _minPitch = 0.9f,
+                  _maxPitch = 1.1f;
 
     private void OnDestroy()
     {
@@ -12,6 +16,9 @@ public class WaterDrop : MonoBehaviour
         _deathEffect.gameObject.SetActive(true);
         _deathEffect.transform.SetParent(null);
         _deathEffect.transform.localScale = new Vector3(1, 1, 1);
+        _source.enabled = true;
+        _source.pitch = Random.Range(_minPitch, _maxPitch);
+        _source.Play();
         Destroy(_deathEffect, 2);
     }
 }
