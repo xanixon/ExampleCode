f6fd1bde5b59921e09e33ccf7433dca1fbb072e8
xanixon <xanixon@mail.ru>
2024-03-27 14:48:50 +0300

    Updating ServiceInstaller

--- Assets/Resources/ServiceInstaller.cs
+++ Assets/Resources/ServiceInstaller.cs
@@ -6,17 +6,27 @@ public class ServiceInstaller : MonoInstaller
     [SerializeField] private WorldMotor _worldMotor;
     [SerializeField] private SceneObjectsSpawner _decorationSpawner;
     [SerializeField] private SceneObjectsSpawner _waterSpawner;
+    [SerializeField] private SceneObjectsSpawner _obstacleSpawner;
     [SerializeField] private ScoreHandler _scoreHandler;
     [SerializeField] private BaseUIHint _baseUIHint;
     [SerializeField] private CameraController _cameraController;
+    [SerializeField] private GameOverHandler _gameOverHandler;
+    [SerializeField] private PauseHandler _pauseHandler;
+
+    [SerializeField] private BrokenBeerMug _brokenBeer;
 
     public override void InstallBindings()
     {
         Container.Bind<WorldMotor>().FromComponentInNewPrefab(_worldMotor).AsSingle().Lazy();
         Container.Bind<SceneObjectsSpawner>().WithId("DecorationsSpawner").FromComponentInNewPrefab(_decorationSpawner).AsCached().Lazy();
         Container.Bind<SceneObjectsSpawner>().WithId("WaterSpawner").FromComponentInNewPrefab(_waterSpawner).AsCached().Lazy();
+        Container.Bind<SceneObjectsSpawner>().WithId("ObstacleSpawner").FromComponentInNewPrefab(_obstacleSpawner).AsCached().Lazy();
         Container.Bind<ScoreHandler>().FromComponentInNewPrefab(_scoreHandler).AsSingle().Lazy();
         Container.Bind<BaseUIHint>().FromComponentInNewPrefab(_baseUIHint).AsSingle().Lazy();
         Container.Bind<CameraController>().FromComponentInNewPrefab(_cameraController).AsSingle().Lazy();
+        Container.Bind<GameOverHandler>().FromComponentInNewPrefab(_gameOverHandler).AsSingle().Lazy();
+        Container.Bind<GameOverHandler>().FromComponentInNewPrefab(_pauseHandler).AsSingle().Lazy();
+
+        Container.BindFactory<BrokenBeerMug, BrokenBeerMug.Factory>().FromComponentInNewPrefab(_brokenBeer).AsSingle();
     }
 }
